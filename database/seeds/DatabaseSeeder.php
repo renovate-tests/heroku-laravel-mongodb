b0aefb639ec237dfeb3e4655f2ea54b5757b7fe9
guy <ambar.guy@gmail.com>
2017-08-29 13:46:27 +0300

    added user seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        factory(App\User::class)->create();
+        factory(App\User::class)->create(['email' => 'mail@mail.com']);
     }
 }
