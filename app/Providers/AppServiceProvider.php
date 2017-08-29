805f9bc3dee94a86df0c6aac9f75e56cbc182dfe
guy <ambar.guy@gmail.com>
2017-08-29 13:52:34 +0300

    fixed mongodb getName

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Moloquent\Eloquent\Builder;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        Builder::macro('getName', function() {
+            return 'mongodb';
+        });
     }
 }
